75ea2b3ededaaae000aaf4748641c209721bd49b
Metcela-Code <65157641+Metcela-Code@users.noreply.github.com>
2020-07-10 11:18:03 +0900

    Update PCA with graph and ranking.R
    Removal of names of input files to make a ready to use code.

--- PCA with graph and ranking.R
+++ PCA with graph and ranking.R
@@ -1,17 +1,17 @@
 # Loading of additional packages
 library(ggplot2)
 
-# Definition of working directory
-setwd(".")
+# Definition of the working directory
+setwd("")
 
-# Definition of data file
-data.matrix <- read.table("Group_FVNCF-VS-FVCF_DE_significant_anno - up and down.txt",
+# Definition of the data file
+data.matrix <- read.table(".txt",
                           header = TRUE)
 
 # PCA on input data
 pca <- prcomp(t(data.matrix), scale = TRUE)
 
-# Definition of data for scree plot
+# Definition of data for the scree plot
 pca.var <- pca$sdev ^ 2
 pca.var.per <- round(pca.var / sum(pca.var) * 100, 1)
 scree.data <- data.frame(Component = c(colnames(pca$x)), Percent = pca.var.per)
@@ -24,15 +24,15 @@ ggplot(data = scree.data, aes(x = Component, y = Percent)) +
   geom_text(aes(label = paste(Percent, "%", sep = ""), vjust = -0.3)) +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))+
-  ggtitle("Scree plot for genes upregulated in fVCF")
+  ggtitle("Scree plot")
 
-# Definition of data for PCA plot
+# Definition of data for the PCA plot
 pca.data <- data.frame(Sample = rownames(pca$x),
                        X = pca$x[,1], # PC1
                        Y = pca$x[,2]) # PC2
 
-# Alternative names for PCA plot
-pca.data["CellType"] <- c("VNCF", "VNCF", "VNCF", "VCF", "VCF", "VCF")
+# Explicit names of samples for the PCA plot
+pca.data["CellType"] <- c("Type 1", "Type 1", "Type 2", "Type 2")
 pca.data$CellType <-as.factor(pca.data$CellType)
 
 # PCA plot
@@ -43,14 +43,14 @@ ggplot(data = pca.data, aes(x = X, y = Y, fill = CellType)) +
   theme(legend.position="right", legend.title = element_blank()) +
   xlab(paste("PC1 - ", pca.var.per[1], "%", sep = "")) +
   ylab(paste("PC2 - ", pca.var.per[2], "%", sep = "")) +
-  ggtitle("PCA for genes upregulated in fVCF")
+  ggtitle("PCA plot")
 
 # Ranking of genes contributing most to PC1
 loading_scores <- pca$rotation[,1] # rotation is an output of prcomp()
 gene_scores <- abs(loading_scores) # see: magnitudes
 gene_score_ranked <- sort(gene_scores, decreasing=TRUE)
-genes_ranking <- names(gene_score_ranked[1:2286])
+genes_ranking <- names(gene_score_ranked["START":"END"])
 
-# Print genes and scores
+# Save genes and scores
 pca$rotation[genes_ranking, 1]
 write.csv(pca$rotation[genes_ranking, 1], file = "genes_ranking.csv")
